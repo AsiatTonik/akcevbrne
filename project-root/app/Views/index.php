6eede00acd8d4938e5f53d141718a6a8b80ab6a8
AsiatTonik <phamhongphuoc2005@gmail.com>
2024-12-08 19:03:50 +0100

    zmena

--- project-root/app/Views/index.php
+++ project-root/app/Views/index.php
@@ -1,21 +1,12 @@
+<?= $this->extend('layout/layout'); ?> 
+<?= $this->section('content'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Akce v Brně</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/styling.css') ?>">
-</head>
+
 <body>
     <div class="hlavni-container">
-        <header class="hlavni-header">
-            <h1>AKCE V BRNĚ</h1>
-            <a href="http://localhost/pham/project-root/login">
-            <button class="login-button">LOGIN</button>
-            </a>
-
-        </header>
+       
         <aside class="sidebar">
             <ul>
                 <li>NÁZEV KATEGORIE</li>
@@ -48,18 +39,11 @@
         </main>
 
         
-        <footer class="hlavni-footer">
-    <div class="footer-content">
         
-        <div class="social-links">
-            <a href="https://www.facebook.com" target="_blank">Facebook</a> |
-            <a href="https://www.twitter.com" target="_blank">Twitter</a> |
-            <a href="https://www.instagram.com" target="_blank">Instagram</a>
-        </div>
-    </div>
-</footer>
 
 
     </div>
 </body>
 </html>
+
+<?= $this->endSection(); ?> 
